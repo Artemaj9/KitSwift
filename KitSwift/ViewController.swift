318bf4dee9af871b5111764177b752a3c0180857
Artem Moiseev <moiseev@physics.msu.ru>
2023-11-10 08:32:52 +0300

    UI Kit preview

--- KitSwift/ViewController.swift
+++ KitSwift/ViewController.swift
@@ -5,15 +5,41 @@
 //  Created by Artem on 10.11.2023.
 //
 
+import SwiftUI
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let awesome = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+        view.addSubview(awesome)
+        awesome.backgroundColor = .purple
+        awesome.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+          awesome.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+          awesome.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100),
+          awesome.widthAnchor.constraint(equalToConstant: 250),
+          awesome.heightAnchor.constraint(equalToConstant: 250)
+        ])
     }
+}
 
+struct KitCrash: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        ViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+}
 
+struct ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+            KitCrash()
+                .ignoresSafeArea()
+    }
 }
 
